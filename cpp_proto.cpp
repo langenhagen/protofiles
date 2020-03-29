c1e339081f499df4fc25e262e5ad6aa602db648a
Andreas Langenhagen <andreas@langenhagen.cc>
2020-03-29 21:35:24 +0200

    Add cpp_proto.cpp
    Add cpp_proto.cpp. I see, I was away from c++ to remember many things
    from the top of my head.

---
+++ cpp_proto.cpp
@@ -0,0 +1,5 @@
+// -------------------------------------------------------------------------------------------------
+// int const* is equivalent to const int*.
+
+const int i = 23;   // works
+int const j = 121;  // works, too
