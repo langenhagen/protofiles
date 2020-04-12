4a200450a365dce875a67f038416c72da08b1648
Andreas Langenhagen <andreas@langenhagen.cc>
2020-04-12 11:17:34 +0200

    Add a variables section to fish_proto.fish

--- fish_proto.fish
+++ fish_proto.fish
@@ -9,3 +9,10 @@ end
 for a in (abbr --list)
     abbr --erase $a
 end
+
+# --------------------------------------------------------------------------------------------------
+# variables
+
+set myvar 23
+
+set -lx PIP_REQUIRE_VIRTUALENV false  # temporary env var; -l|--local: local -x|--export: export;
