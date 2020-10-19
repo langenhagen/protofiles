a905b907c0492c0b9daeef10bf5c1663985d1a12
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-10-19 17:57:11 +0200

    Update jq_proto.sh

--- jq_proto.sh
+++ jq_proto.sh
@@ -2,12 +2,30 @@
 #
 # author: andreasl
 
-# select stuff and filter
-cat 'myfile.json' | jq -r '.[] | select(.state == "some_state") | "\(.loc), \(.sku)"'
+###############################################################################
+# command line args
 
+# jq -r   # don't parse input as json, instead, each line is passed to the filter as a string.
+
+###############################################################################
+# select stuff and filter
+
+cat myfile.json | jq '.[] | select(.state == "some_state") | "\(.loc), \(.sku)"'
+
+###############################################################################
 # get distinct values
-cat myfile.json | jq -r 'map(.myfield) | unique'  # jq-ish way
-cat myfile.json | jq -r '.[] | .state' | sort -u  # bash-y way
+cat myfile.json | jq 'map(.myfield) | unique'  # jq-ish way
+cat myfile.json | jq '.[] | .state' | sort -u  # bash-y way
+
+###############################################################################
+# count
 
 # count array lenghts
-cat myarrayfile.json | jq -r 'map(.myfield) | unique | length'
+cat myarrayfile.json | jq 'map(.myfield) | unique | length'
+
+
+###############################################################################
+# grouping
+
+# group all json objects with the same value for `myfield` into arrays, yielding several arrays
+cat myarrayfile.json| jq 'group_by(.myfield)'
