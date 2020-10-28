d479940b40074355d725af9ff43f8b0b223bf309
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-10-28 18:20:30 +0100

    Update sql_proto.sql

--- sql_proto.sql
+++ sql_proto.sql
@@ -4,15 +4,52 @@
 -- that's a single line comment
 
 -------------------------------------------------------------------------------
+-- the interactive fundamentals
+
+show databases
+
+use mydatabase  -- also important in sql scripts
+
+show tables
+
+DESCRIBE mytable  -- show meta information about the table
+DESC mytable  -- short form of DESCRIBE mytable
+
+-------------------------------------------------------------------------------
 -- query data
 
-SELECT * FROM my_table
-SELECT mycol1, mycol3 FROM my_table
-SELECT mt.mycol1, mt.mycol3 FROM my_table mt
+SELECT * FROM mytable
+SELECT mycol1, mycol3 FROM mytable
+SELECT mt.mycol1, mt.mycol3 FROM mytable mt
 
-SELECT mycol1, mycol3 FROM my_table WHERE mycol2 = "some value"
+SELECT mycol1, mycol3 FROM mytable WHERE mycol2 = "some value"
+
+SELECT * FROM mytable WHERE mycol2 = "some value" AND myothercol != 42
+
+SELECT * FROM mytable WHERE mycol LIKE "%jack%jones%"   -- case insensitive
+
 
 -------------------------------------------------------------------------------
 -- get all distinct values in a column
 
 SELECT DISTINCT mt.mycol FROM mytable mt WHERE mt.myothercol = "miau"
+
+
+-------------------------------------------------------------------------------
+-- variables
+
+SET @my_variable = "%jack%jones%";
+
+SELECT * FROM my_brands abb WHERE brand LIKE @my_variable;
+
+
+-------------------------------------------------------------------------------
+-- instertion of rows
+
+INSERT INTO mytable (brand, pic_url) VALUES ('Wildling','Wildling.png')
+
+-------------------------------------------------------------------------------
+-- deletion
+
+DELETE FROM mytable                -- delete everything from mytable
+DELETE FROM mytable mt WHERE mt.mycol = "nooo"
