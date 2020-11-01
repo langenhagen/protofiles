16b9574fd8d0d967e0ef482e0b086b71fda7b317
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-11-02 00:55:52 +0100

    Update bash_proto.sh

--- bash_proto.sh
+++ bash_proto.sh
@@ -156,6 +156,9 @@ echo "${my_var//Andi/}"  # remove all occurences of Andi, does not overwrite the
 echo "${my_var/Andi/Cat}"  # replace first occurence of Andi with Cat, does not overwrite the original var
 echo "${my_var//Andi/Cat}"  # replace all occurences of Andi with Cat, does not overwrite the original var
 
+echo "${$my_multiline_string//$'\n'/,}"  # replace all newlines with commas ,
+
+
 echo "${my_var#Hello, }"  # nongreedy remove any prefix from the expanded value that matches the pattern
 echo "${my_var%, Andi and Andi}"  # nongreedy remove any suffx from the expanded value that matches the pattern
 
