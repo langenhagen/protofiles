8979880fd987e0e79e5214ef3dd40f6c85ee3448
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-12-10 21:33:46 +0100

    Rephrase a prompt in bash_proto.sh

--- bash_proto.sh
+++ bash_proto.sh
@@ -1082,7 +1082,7 @@ trap "read -n1 -p 'Press any key to exit' -s; echo" EXIT
 
 
 printf 'Really?\n'
-read -r -p 'Press <ctrl+c> to escape or press <Enter> to proceed'
+read -r -p 'Press [ENTER] to continue or Ctrl-c to cancel.'
 
 
 # --------------------------------------------------------------------------------------------------
