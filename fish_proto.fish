8a4eff7a69ac349afdd25b9e899894250ded13df
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-02-06 02:06:49 +0100

    Extend fish_proto.fish

--- fish_proto.fish
+++ fish_proto.fish
@@ -30,3 +30,15 @@ end
 set myvar 23
 
 set -lx PIP_REQUIRE_VIRTUALENV false  # temporary env var; -l|--local: local -x|--export: export;
+
+
+# --------------------------------------------------------------------------------------------------
+# functions
+
+function say_hi
+    echo "Hi $argv"
+end
+
+say_hi
+say_hi everbody!
+say_hi you and you and you
